664aef11a569d3191f3e3f972531b11b9375c45d
Nisarg Raval <nisargraval95@gmail.com>
2020-12-06 11:24:04 +0530

    Add solution for day6

---
+++ 2020/day6/day6.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	groupAnswers, err := readGroupAnswers("input.txt")
+	if err != nil {
+		panic(fmt.Errorf("readGroupAnswers threw %v\n", err))
+	}
+	fmt.Println(computePartOne(groupAnswers))
+	fmt.Println(computePartTwo(groupAnswers))
+}
+
+func computePartOne(groupAnswers []string) int {
+	numAnswers := 0
+	for _, groupAnswer := range groupAnswers {
+		answersFromEveryone := strings.Split(groupAnswer, "\n")
+		numAnswers += computeYesAnswersFromAnyone(answersFromEveryone)
+	}
+	return numAnswers
+}
+
+func computePartTwo(groupAnswers []string) int {
+	numAnswers := 0
+	for _, groupAnswer := range groupAnswers {
+		answersFromEveryone := strings.Split(groupAnswer, "\n")
+		numAnswers += computeYesAnswersFromEveryone(answersFromEveryone)
+	}
+	return numAnswers
+}
+
+func computeYesAnswersFromEveryone(answersFromEveryone []string) int {
+	numYesAnswersByEveryone := 0
+	answerToCount := make(map[string]int)
+
+	for _, answersFromPerson :=range answersFromEveryone {
+		answers := strings.Split(answersFromPerson, "")
+		for _, answer := range answers {
+			if _, ok := answerToCount[answer];  ok {
+				answerToCount[answer]++
+			} else {
+				answerToCount[answer] = 1
+			}
+		}
+	}
+	for _, count := range answerToCount {
+		if count == len(answersFromEveryone) {
+			numYesAnswersByEveryone++
+		}
+	}
+	return numYesAnswersByEveryone
+}
+
+func computeYesAnswersFromAnyone(answersFromEveryone []string) int {
+	answeredYesMap := make(map[string]bool)
+
+	for _, answersFromPerson :=range answersFromEveryone {
+		answers := strings.Split(answersFromPerson, "")
+		for _, answer := range answers {
+			if _, ok := answeredYesMap[answer];  !ok {
+				answeredYesMap[answer] = true
+			}
+		}
+	}
+	return len(answeredYesMap)
+}
+
+func readGroupAnswers(inputFileName string) ([]string, error) {
+	inputFile, err := os.Open(inputFileName)
+	if err != nil {
+		return nil, err
+	}
+	defer inputFile.Close()
+
+	content, err := ioutil.ReadAll(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(content), "\n\n"), nil
+}
