1cc0d2cc2e456a4ea93b5a84e6572364d7120ff2
Nisarg Raval <nisargraval95@gmail.com>
2020-12-15 11:39:55 +0530

    Add solution for day 15

---
+++ 2020/day15/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Problem: https://adventofcode.com/2020/day/15
+func main() {
+	startingNumbers := []int{6, 19, 0, 5, 7, 13, 1}
+	fmt.Println(computePartOne(startingNumbers, 2020))
+	fmt.Println(computePartTwo(startingNumbers))
+}
+
+// This is a pretty elegant solution in my opinion.
+// I had initially solved part 1 by storing all the indices where a number was previously spoken
+// However, that is catastrophic for part 2 given its input size. The main bottleneck was the copy() function.
+// So, I decided to only always store the LAST TWO indices where a number was previously spoken. VOILA! No copy() and runs FAST!
+func computePartOne(nums []int, lenSpokenNumbers int) int {
+	spokenNumbers := make([]int, lenSpokenNumbers)
+	numberToSpokenBeforeIndices := make(map[int][]int)
+
+	// Add all starting numbers to spokenNumbers first
+	for i := 0; i < len(nums); i++ {
+		spokenNumbers[i] = nums[i]
+		numberToSpokenBeforeIndices[nums[i]] = []int{i}
+	}
+
+	// Main loop
+	for i := len(nums); i < len(spokenNumbers); i++ {
+		lastNumberSpoken := spokenNumbers[i-1]
+		spokenBeforeIndices, ok := numberToSpokenBeforeIndices[lastNumberSpoken]
+		var currSpokenNumber int
+
+		if ok && len(spokenBeforeIndices) > 1 {
+			// if spoken before
+			lastIndex := spokenBeforeIndices[1]
+			secondLastIndex := spokenBeforeIndices[0]
+			currSpokenNumber = lastIndex - secondLastIndex
+			spokenNumbers[i] = currSpokenNumber
+		} else {
+			//	not spoken before
+			currSpokenNumber = 0
+			spokenNumbers[i] = currSpokenNumber
+		}
+		// Update mapping for spokenNumber
+		currSpokenNumberSpokenBeforeIndices, ok := numberToSpokenBeforeIndices[currSpokenNumber]
+		if ok {
+			// only append last two
+			numberToSpokenBeforeIndices[currSpokenNumber] = append(
+				[]int{currSpokenNumberSpokenBeforeIndices[len(currSpokenNumberSpokenBeforeIndices)-1], i},
+			)
+		} else {
+			numberToSpokenBeforeIndices[currSpokenNumber] = []int{i}
+		}
+	}
+	return spokenNumbers[lenSpokenNumbers-1]
+}
+
+func computePartTwo(nums []int) int {
+	return computePartOne(nums, 30000000)
+}
